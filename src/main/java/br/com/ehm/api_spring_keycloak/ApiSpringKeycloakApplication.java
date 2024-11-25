596530aae97c18b30d992b6a6123cb6e87992db5
hoshinoehm <hoshinoehm@gmail.com>
2024-11-25 16:54:15 -0300

    first commit

---
+++ src/main/java/br/com/ehm/api_spring_keycloak/ApiSpringKeycloakApplication.java
@@ -0,0 +1,13 @@
+package br.com.ehm.api_spring_keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringKeycloakApplication.class, args);
+	}
+
+}
